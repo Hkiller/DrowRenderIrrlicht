8a242fe23b1dbf7931284db761f837e81d56f043
Loki <wangjian1009@gmail.com>
2016-09-21 20:03:42 +0800

    增加xcode工程

---
+++ include/irr_runtime/irr_runtime_types.h
@@ -0,0 +1,15 @@
+#ifndef IRR_RUNTIME_TYPES_H
+#define IRR_RUNTIME_TYPES_H
+#include "render/runtime/ui_runtime_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct irr_runtime_module * irr_runtime_module_t;
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif
